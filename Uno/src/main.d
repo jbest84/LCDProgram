1e5dcc518c0718ef07705f6c169b9f30497b9c60
Jason Best <skeeterbug@gmail.com>
2014-07-05 23:03:37 -0700

    Initial commit

---
+++ Uno/src/main.d
@@ -0,0 +1,33 @@
+src/main.d src/main.o: ../src/main.cpp ../src/main.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/Arduino.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/binary.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/WCharacter.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/WString.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/HardwareSerial.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/Stream.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/Print.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/Printable.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/new.h \
+  C:\Users\Jason\workspace\ArduinoCore\src/pins_arduino.h
+
+../src/main.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/Arduino.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/binary.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/WCharacter.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/WString.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/HardwareSerial.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/Stream.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/Print.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/Printable.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/new.h:
+
+C:\Users\Jason\workspace\ArduinoCore\src/pins_arduino.h:
---
+++ Uno/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/main.cpp 
+
+OBJS += \
+./src/main.o 
+
+CPP_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"C:\Users\Jason\workspace\ArduinoCore\src" -Wall -g2 -gstabs -Os -ffunction-sections -fdata-sections -ffunction-sections -fdata-sections -fno-exceptions --pedantic -mmcu=atmega328p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
